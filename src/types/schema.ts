f070d962d5e798d63799e491314769cf3795c977
dabit3 <dabit3@gmail.com>
2021-06-09 00:24:19 -0500

    updated subgraph to include marketplace

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -85,21 +85,13 @@ export class Token extends Entity {
     }
   }
 
-  get name(): string | null {
+  get name(): string {
     let value = this.get("name");
-    if (value === null || value.kind == ValueKind.NULL) {
-      return null;
-    } else {
-      return value.toString();
-    }
+    return value.toString();
   }
 
-  set name(value: string | null) {
-    if (value === null) {
-      this.unset("name");
-    } else {
-      this.set("name", Value.fromString(value as string));
-    }
+  set name(value: string) {
+    this.set("name", Value.fromString(value));
   }
 
   get creator(): string {
